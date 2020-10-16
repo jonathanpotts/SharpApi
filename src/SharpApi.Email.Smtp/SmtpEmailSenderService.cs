77b597392edc1c7a4f199311f4bceb7a57f4ac0e
Jonathan Potts <jonathanpotts@outlook.com>
2020-10-16 15:39:28 -0700

    Start implementing SMTP service

---
+++ src/SharpApi.Email.Smtp/SmtpEmailSenderService.cs
@@ -0,0 +1,23 @@
+﻿using MimeKit;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace SharpApi.Email.Smtp
+{
+    /// <summary>
+    /// Sends emails via SMTP.
+    /// </summary>
+    public class SmtpEmailSenderService : IEmailSenderService
+    {
+        public async Task SendAsync(MailMessage message)
+        {
+            var mimeMessage = (MimeMessage)message;
+
+            using var smtpClient = new SmtpClient();
+            await smtpClient.SendAsync(mimeMessage);
+            await smtpClient.DisconnectAsync(true);
+        }
+    }
+}
---
+++ src/SharpApi.Email/IEmailSenderService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SharpApi.Email
+{
+    /// <summary>
+    /// Interface used to implement email sender services.
+    /// </summary>
+    public interface IEmailSenderService
+    {
+        /// <summary>
+        /// Sends the provided email message.
+        /// </summary>
+        /// <param name="message">Email message to send.</param>
+        /// <returns>Task representing the status of sending the email.</returns>
+        public Task SendAsync(MailMessage message);
+    }
+}
