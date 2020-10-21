2a761e5d6cb2d06abdafd581cde582deda85977a
Jonathan Potts <jonathanpotts@outlook.com>
2020-10-21 12:14:59 -0700

    Add doc xref service

--- src/SharpApi/ApiEndpoint.cs
+++ src/SharpApi/ApiEndpoint.cs
@@ -25,8 +25,8 @@ namespace SharpApi
     /// <summary>
     /// An endpoint that handles API requests.
     /// </summary>
-    /// <typeparam name="T">Model type for body data.</typeparam>
-    public abstract class ApiEndpoint<T> : ApiEndpoint
+    /// <typeparam name="TModel">Model type for body data.</typeparam>
+    public abstract class ApiEndpoint<TModel> : ApiEndpoint
     {
         /// <summary>
         /// Creates a new instance of the endpoint.
@@ -41,8 +41,8 @@ namespace SharpApi
         /// </summary>
         /// <param name="request">API request data.</param>
         /// <returns>API response data.</returns>
-        public abstract Task<ApiResult> RunAsync(ApiRequest<T> request);
+        public abstract Task<ApiResult> RunAsync(ApiRequest<TModel> request);
 
-        public override Task<ApiResult> RunAsync(ApiRequest request) => RunAsync(new ApiRequest<T>(request));
+        public override Task<ApiResult> RunAsync(ApiRequest request) => RunAsync(new ApiRequest<TModel>(request));
     }
 }
